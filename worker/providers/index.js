3c3c3256d8e8941da1583ec503fba4a4332a4dc5
Shahnewaj Mahmood <69941899+shomudraa@users.noreply.github.com>
2026-09-18 20:01:59 +0600

    Enable PROVIDER=google for Omni Flash testing

--- worker/providers/index.js
+++ worker/providers/index.js
@@ -5,13 +5,14 @@ import * as vmodel from "./vmodel.js";
 import * as akool from "./akool.js";
 import * as charreplace from "./charreplace.js";
 import * as falai from "./falai.js";
+import * as google from "./google.js";
 
 /**
  * Every provider exports:
  *   swapVideo({ videoPath, facePath, outPath, log }) -> Promise<outPath>
  * It must download the finished swapped video to outPath.
  */
-const providers = { mock, magichour, vmodel, akool, charreplace, falai };
+const providers = { mock, magichour, vmodel, akool, charreplace, falai, google };
 
 export function getProvider(name = config.provider) {
   const p = providers[name];
