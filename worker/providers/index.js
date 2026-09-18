3a69038b018a9c6191d4f45ff9b8d091f6bb7f3f
Shahnewaj Mahmood <69941899+shomudraa@users.noreply.github.com>
2026-09-18 17:58:52 +0600

    Add files via upload

--- worker/providers/index.js
+++ worker/providers/index.js
@@ -3,7 +3,6 @@ import * as mock from "./mock.js";
 import * as magichour from "./magichour.js";
 import * as vmodel from "./vmodel.js";
 import * as akool from "./akool.js";
-import * as generate from "./generate.js";
 import * as charreplace from "./charreplace.js";
 import * as falai from "./falai.js";
 
@@ -12,7 +11,7 @@ import * as falai from "./falai.js";
  *   swapVideo({ videoPath, facePath, outPath, log }) -> Promise<outPath>
  * It must download the finished swapped video to outPath.
  */
-const providers = { mock, magichour, vmodel, akool, generate, charreplace, falai };
+const providers = { mock, magichour, vmodel, akool, charreplace, falai };
 
 export function getProvider(name = config.provider) {
   const p = providers[name];
